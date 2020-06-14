b8aec547077d6a48a4f8f917caaae36f49f90eb9
Roger Williams <53829190+williamr12@users.noreply.github.com>
2020-06-14 17:33:46 -0500

    Update Soldier.java
    added id argument and setId() method to constructor.

--- ZombieWar/src/main/java/com/csc422/zombiewar/Soldier.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Soldier.java
@@ -2,9 +2,9 @@ package com.csc422.zombiewar;
 
 public class Soldier extends Survivor {
 
-    public Soldier( ) {
+    public Soldier(int id ) {
         super(/*health*/ 100,/*attack*/ 10);
-
+        setId(id);
     }
 
 }
