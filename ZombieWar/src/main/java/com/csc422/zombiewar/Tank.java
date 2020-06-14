49d49cd2283b04b3b7f4184cb440eb6911461f31
Roger Williams <53829190+williamr12@users.noreply.github.com>
2020-06-14 17:35:35 -0500

    Update Tank.java
    added id argument and set(id) method to constructor.

--- ZombieWar/src/main/java/com/csc422/zombiewar/Tank.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Tank.java
@@ -2,9 +2,9 @@ package com.csc422.zombiewar;
 
 public class Tank extends Zombie {
 
-    public Tank( ) {
+    public Tank(int id) {
         super(/*health*/ 150,/*attack*/ 20);
-
+        setId(id);
     }
 
 }
