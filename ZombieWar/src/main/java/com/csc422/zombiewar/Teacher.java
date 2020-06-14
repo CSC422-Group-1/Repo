700c23a797d9930070c5d301ccf06937d63d3f9d
Roger Williams <53829190+williamr12@users.noreply.github.com>
2020-06-14 17:36:54 -0500

    Update Teacher.java
    Added int id argument and setId(id) method to constructor.

--- ZombieWar/src/main/java/com/csc422/zombiewar/Teacher.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Teacher.java
@@ -2,9 +2,9 @@ package com.csc422.zombiewar;
 
 public class Teacher extends Survivor {
 
-    public Teacher( ) {
+    public Teacher(int id) {
         super(/*health*/ 50,/*attack*/ 5);
-
+        set(id);
     }
 
 }
