d9685f7196410697e36577da2397a83f0bf2b3aa
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-06-14 16:25:07 -0500

    toString and ID count

--- ZombieWar/src/main/java/com/csc422/zombiewar/Child.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Child.java
@@ -1,10 +1,19 @@
 package com.csc422.zombiewar;
 
-public class Child extends Survivor {
-
-    public Child( ) {
+public class Child extends Survivor
+{
+    private int objID = 0;
+    
+    public Child(int ID)
+    {
         super(/*health*/ 20,/*attack*/ 2);
-
+        this.objID = ID;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Child " + objID;
     }
 
 }
