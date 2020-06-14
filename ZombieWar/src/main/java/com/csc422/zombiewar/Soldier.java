d8d255fe56d620f94fd1ccbb6710557c218c5e2f
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-06-14 16:26:02 -0500

    Update Soldier.java

--- ZombieWar/src/main/java/com/csc422/zombiewar/Soldier.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Soldier.java
@@ -1,10 +1,20 @@
 package com.csc422.zombiewar;
 
-public class Soldier extends Survivor {
+public class Soldier extends Survivor
+{
+    private int objID = 0;
+    
+    public Soldier(int ID)
+    {
+        super(/*health*/100,/*attack*/ 10);
+        this.objID = ID;
+    }
 
-    public Soldier( ) {
-        super(/*health*/ 100,/*attack*/ 10);
-
+    @Override
+    public String toString()
+    {
+        return "Soldier " + objID;
     }
 
 }
+
