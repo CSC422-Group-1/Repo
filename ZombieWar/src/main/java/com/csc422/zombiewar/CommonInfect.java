c7fbd3baa0b8c05688dbccb3b06c57cce7750b93
Roger Williams <53829190+williamr12@users.noreply.github.com>
2020-06-14 17:24:18 -0500

    Update CommonInfect.java
    added int id constructor. and added setId() method.

--- ZombieWar/src/main/java/com/csc422/zombiewar/CommonInfect.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/CommonInfect.java
@@ -2,8 +2,10 @@ package com.csc422.zombiewar;
 
 public class CommonInfect extends Zombie {
 
-    public CommonInfect( ) {
+    public CommonInfect(int id) {
         super(/*health*/ 30,/*attack*/ 5);
+        this.id =id;
+        setId(id);
 
     }
 
