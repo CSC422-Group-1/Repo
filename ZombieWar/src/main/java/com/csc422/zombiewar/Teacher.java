739534229d19f1f65a830e7bc08d11b56bd8c52d
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-06-14 16:27:15 -0500

    Update Teacher.java

--- ZombieWar/src/main/java/com/csc422/zombiewar/Teacher.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Teacher.java
@@ -1,10 +1,19 @@
 package com.csc422.zombiewar;
 
-public class Teacher extends Survivor {
+public class Teacher extends Survivor
+{
+    private int objID = 0;
 
-    public Teacher( ) {
-        super(/*health*/ 50,/*attack*/ 5);
+    public Teacher(int ID)
+    {
+        super(/*health*/50,/*attack*/ 5);
+        this.objID = ID;
+    }
 
+    @Override
+    public String toString()
+    {
+        return "Teacher " + objID;
     }
 
 }
