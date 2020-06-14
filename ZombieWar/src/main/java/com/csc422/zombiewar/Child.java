58317a4c4cd4a39438fff8d3e901acd8d2d9f3f1
Roger Williams <53829190+williamr12@users.noreply.github.com>
2020-06-14 17:23:17 -0500

    Update Child.java
    changed constructor to get int id, and setId(id).

--- ZombieWar/src/main/java/com/csc422/zombiewar/Child.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/Child.java
@@ -2,9 +2,10 @@ package com.csc422.zombiewar;
 
 public class Child extends Survivor {
 
-    public Child( ) {
+    public Child(int id) {
         super(/*health*/ 20,/*attack*/ 2);
-
+        this.id =id;
+        setId(id);
     }
 
 }
