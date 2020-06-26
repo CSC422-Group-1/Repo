a4ee801ad42021f02f58ef4c21cad258d3d84377
PGarryCS <65678578+PGarryCS@users.noreply.github.com>
2020-06-26 18:46:41 -0500

    Survivor; Child; Teacher; Soldier
    Survivor; Child; Teacher; Soldier

--- ZombieWar/src/main/java/com/csc422/zombiewar/characters/Child.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/characters/Child.java
@@ -1,12 +1,20 @@
 package com.csc422.zombiewar.characters;
 
+import com.csc422.zombiewar.weapons.Weapon;
+
 public class Child extends Survivor
 {
     private int objID = 0;
     
+    public Child(int ID, Weapon weapon)
+    {
+        super(/*health*/20,/*attack*/2, weapon);
+        this.objID = ID;
+    }
+    
     public Child(int ID)
     {
-        super(/*health*/ 20,/*attack*/ 2);
+        super(/*health*/ 20,/*attack*/2);
         this.objID = ID;
     }
     
