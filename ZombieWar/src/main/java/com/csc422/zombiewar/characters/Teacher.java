e3a14cb89cd68c7893ca37724cf2fe7763f72100
WILLIRX19 <WILLIRX19@USMN-FL3H733-L.oneabbott.com>
2020-06-27 17:05:17 -0500

    Removed toString method from instance

--- ZombieWar/src/main/java/com/csc422/zombiewar/characters/Teacher.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/characters/Teacher.java
@@ -1,19 +1,21 @@
 package com.csc422.zombiewar.characters;
 
+import com.csc422.zombiewar.weapons.Weapon;
+
 public class Teacher extends Survivor
 {
     private int objID = 0;
-
-    public Teacher(int ID)
+    
+    public Teacher(int ID, Weapon weapon)
     {
-        super(/*health*/50,/*attack*/ 5);
+        super(/*health*/50,/*attack*/5, weapon);
         this.objID = ID;
     }
 
-    @Override
-    public String toString()
+    public Teacher(int ID)
     {
-        return "Teacher " + objID;
+        super(/*health*/50,/*attack*/5);
+        this.objID = ID;
     }
 
 }
