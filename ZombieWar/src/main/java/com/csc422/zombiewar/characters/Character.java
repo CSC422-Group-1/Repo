5029a94068a3452fdf9fe833dfec4be25e195411
WILLIRX19 <WILLIRX19@USMN-FL3H733-L.oneabbott.com>
2020-06-27 17:02:40 -0500

    Updated Character class for weapons.

--- ZombieWar/src/main/java/com/csc422/zombiewar/characters/Character.java
+++ ZombieWar/src/main/java/com/csc422/zombiewar/characters/Character.java
@@ -1,5 +1,7 @@
 package com.csc422.zombiewar.characters;
 
+import com.csc422.zombiewar.weapons.Weapon;
+
 /**
  *y
  * @authors Roger, Adrian, Patrick, & Caitlin Course: CSC422 Software
@@ -10,6 +12,8 @@ public abstract class Character
 
     private int health;
     private int attack;
+    private Weapon weapon;
+    private int objID;
 
     /**
      * Constructor
@@ -17,10 +21,23 @@ public abstract class Character
      * @param health
      * @param attack
      */
-    public Character(int health, int attack)
+    
+    public Character(int health, int attack){
+        this.health=health;
+        this.attack=attack;
+    } 
+    /**
+     * Constructor
+     * 
+     * @param health
+     * @param attack
+     * @param weapon
+     */
+    public Character(int health, int attack, Weapon weapon)
     {
         this.health = health;
         this.attack = attack;
+        this.weapon = weapon;
     }
 
     /**
@@ -59,6 +76,10 @@ public abstract class Character
         return attack;
     }
 
+    public Weapon getWeapon(){
+      return weapon;
+    }
+
     /**
      * Simulates this.character attacking another character It reduces the
      * attacked characters health by the attack value of the attacker and sets
@@ -68,16 +89,46 @@ public abstract class Character
      */
     public void attacks(Character c)
     {
-        c.setHealth((c.getHealth()) - (this.attack));
+      if(weapon!=null){
+        if(landedHit()==true){
+          if((weapon.getClass().getSimpleName().equals("FryingPan")|weapon.getClass().getSimpleName().equals("CrowBar")|weapon.getClass().getSimpleName().equals("Axe"))){
+              this.attack=weapon.getDamage();
+          }else{
+              this.attack+=weapon.getDamage();
+          }
+      }
+      c.setHealth((c.getHealth()) - (this.attack));
+    }
     }
 
     /* @return true if this character is dead, otherwise return false */
     public boolean isDead()
     {
-        if (getHealth() <= 0)
-        {
-            return true;
-        }
-        return false;
+        return this.getHealth() <= 0;
     }
+
+    public boolean landedHit(){
+      int modifier =0; //accuracy modifier
+      switch(this.getAttack()){
+        case 10: modifier = 10;
+        case 5: modifier = 45;
+        case 2: modifier = 85;
+      }
+      int rand = (int)(java.lang.Math.random()*weapon.getAccuracy()*10/modifier);
+      //int range = weapon.getAccuracy()+1;
+      return rand!=0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+  public String toString(){
+    String j = this.getClass().getSimpleName()+" "+this.objID;
+    j+=this.getWeapon()!=null?" with ":" ";
+    j+=this.getWeapon()!=null?this.getWeapon().getClass().getSimpleName().startsWith("a")?"an ":"a ":"";
+    j+=this.getWeapon()!=null?this.getWeapon().getClass().getSimpleName():"";
+    return j;
+  }
 }
